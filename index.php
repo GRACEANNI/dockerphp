7db0c10680db5f28d41d3f795892f35125611742
GRACEANNI <ccshinei@hotmail.com>
2023-10-16 05:57:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo phpinfo();
+$data = file_get_contents('php://input');
+
+$logFile = "webhooksentdata.json";
+$log = fopen($logFile,"a");
+fwrite($log,$data);
+fclose($log);
+
+//webhookinside
