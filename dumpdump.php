0bc3a4a35cdefdd05fea6d440b9e01f5b8ebec8d
GRACEANNI <ccshinei@hotmail.com>
2023-10-16 14:58:11 +0800

    Update dumpdump.php

--- dumpdump.php
+++ dumpdump.php
@@ -1,36 +1,48 @@
 <?php
 
-$pathdir = "/app";
-  
-// Enter the name to creating zipped directory 
-$zipcreated = "dumpdump.zip"; 
-  
-// Create new zip class 
-$zip = new ZipArchive; 
-   
-if($zip -> open($zipcreated, ZipArchive::CREATE ) === TRUE) { 
-      
-    // Store the path into the variable 
-    $dir = opendir($pathdir); 
-       
-    while($file = readdir($dir)) { 
-        if(is_file($pathdir.$file)) { 
-          if($pathdir.$file=="dumpdump.zip")continue;
-            $zip -> addFile($pathdir.$file, $file); 
-        } 
-    } 
-    $zip ->close(); 
-} 
+$rootPath = realpath('./');
+$zipcreated = 'dumpdump.zip'; 
+// Initialize archive object
+$zip = new ZipArchive();
+$zip->open($zipcreated, ZipArchive::CREATE | ZipArchive::OVERWRITE);
 
-    if(file_exists($zipcreated)) {
-        // push to download the zip
-        header('Content-type: application/zip');
-        header('Content-Disposition: attachment; filename=file.zip');
-        header('Content-Length: ' . filesize($tmpFile));
-        readfile($zipcreated);
-        // remove zip file is exists in temp path
-        unlink($zipcreated);
-    } else {
-        exit("Error finding file.");
+// Create recursive directory iterator
+/** @var SplFileInfo[] $files */
+$files = new RecursiveIteratorIterator(
+    new RecursiveDirectoryIterator($rootPath),
+    RecursiveIteratorIterator::LEAVES_ONLY
+);
+
+foreach ($files as $name => $file)
+{
+    // Skip directories (they would be added automatically)
+    if (!$file->isDir() && $file!=$zipcreated)
+    {
+        // Get real and relative path for current file
+        $filePath = $file->getRealPath();
+        $relativePath = substr($filePath, strlen($rootPath) + 1);
+
+        // Add current file to archive
+        $zip->addFile($filePath, $relativePath);
+    }
+}
+
+// Zip archive will be created only after closing object
+$zip->close();
+
+
+if(file_exists($zipcreated)) {
+	// push to download the zip
+	header('Content-type: application/zip');
+	header('Content-Disposition: attachment; filename=dump.zip');
+	header('Content-Length: ' . filesize($zipcreated));
+	
+	readfile($zipcreated);
+	
+	// remove zip file is exists in temp path
+	unlink($zipcreated);
+	exit;
+} else {
+	exit("Error zip file.");
     }
   
