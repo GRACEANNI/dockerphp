b6fdfcd4d3e147271cbd71331c5a16f83e1804b8
GRACEANNI <ccshinei@hotmail.com>
2023-10-16 19:56:07 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+include 'config.php';
+
+
 $data = file_get_contents('php://input');
 
 $logFile = "webhooksentdata.json";
@@ -7,4 +10,23 @@ $log = fopen($logFile,"a");
 fwrite($log,$data);
 fclose($log);
 
-//webhookinside
+
+$getdata = json_decode($data.true);
+$userId = $getdata['message']['from']['id'];
+$username = $getdata['message']['from']['username'];
+$usermsg = $getdata['message']['text'];
+
+$botMessage = "你好 ";
+$paraments = array(
+    "chat_id"=>(int)$userId,
+    "text"=>$botMessage.$username,
+    "parse_mode"=>"html"
+);
+
+$apiUrl = "https://api.telegram.org/bot{$BOT_TOKEN}/sendMessage";
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_URL,$apiUrl);
+curl_setopt($ch,CURLOPT_POST,count($paraments));
+curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query($paraments));
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+$result = curl_exec($ch);
