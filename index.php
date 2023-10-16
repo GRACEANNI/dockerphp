c55a122d8079ec071466209ee6e60e95dce840a6
GRACEANNI <ccshinei@hotmail.com>
2023-10-16 21:16:24 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 include 'config.php';
-
+$setuApi = "https://image.anosu.top/pixiv";
 
 $data = file_get_contents('php://input');
 
@@ -11,24 +11,49 @@ fwrite($log,$data);
 fclose($log);
 
 
-$getdata = json_decode($data.true);
-$chatId = $getdata['message']['chat']['id'];
+$getdata = json_decode($data,true);
+$userId = $getdata['message']['from']['id'];
 $username = $getdata['message']['from']['username'];
 $usermsg = $getdata['message']['text'];
-$chatId=(int)$chatId;
+$chatId = $getdata['message']['chat']['id'];
+if($usermsg[0]=='/'){
+
+    $apiUrl = "https://api.telegram.org/bot{$BOT_TOKEN}/sendPhoto?chat_id=".$chatId;
+    $img = 'fake';
+    file_put_contents($img,file_get_contents($setuApi));
+    $paraments = array(
+        "chat_id"=>$chatId,
+        "photo"=>new CURLFile(realpath($img)),
+        //"parse_mode"=>"html"
+    );
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        "Content-Type:multipart/form-data"
+         ));
+    curl_setopt($ch, CURLOPT_URL, $apiUrl); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $paraments); 
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+}else{
+        $botMessage = "你好 ";
+        $paraments = array(
+            "chat_id"=>$userId,
+            "text"=>$botMessage.$username,
+            "parse_mode"=>"html"
+        );
+
+        $apiUrl = "https://api.telegram.org/bot{$BOT_TOKEN}/sendMessage";
+        $ch = curl_init();
+        curl_setopt($ch,CURLOPT_URL,$apiUrl);
+        curl_setopt($ch,CURLOPT_POST,count($paraments));
+        curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query($paraments));
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+}
 
 
-$botMessage = "你好 ";
-$paraments = array(
-    "chat_id"=>$chatId,
-    "text"=>$botMessage.$username,
-    "parse_mode"=>"html"
-);
-
-$apiUrl = "https://api.telegram.org/bot{$BOT_TOKEN}/sendMessage";
-$ch = curl_init();
-curl_setopt($ch,CURLOPT_URL,$apiUrl);
-curl_setopt($ch,CURLOPT_POST,count($paraments));
-curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query($paraments));
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-$result = curl_exec($ch);
